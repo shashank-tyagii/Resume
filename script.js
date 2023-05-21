54661079473c6bccff731b7a370934abceb61aab
Shashank tyagi <shashank.tyagi1234@gmail.com>
2023-05-21 13:44:06 +0530

    new smooth scroll

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
-
+var interval;
 for (let i =0 ; i < navMenuAnchorTags.length; i++){
     navMenuAnchorTags[i].addEventListener('click', function(event){
         event.preventDefault();
         var targetSectionID = this.textContent.trim().toLowerCase();  // Get the name of the section
         var targetSection = document.getElementById(targetSectionID); // Get the section with that Id name
-        var interval = setInterval(function(){
-            var targetSectionCoordinates = targetSection.getBoundingClientRect();
+        interval = setInterval(function(){
+            scrollVertically(targetSection);
+        }, 20);
+
+    });
+}
+
+function scrollVertically(targetSection){
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
             if(targetSectionCoordinates.top <=0){
                 clearInterval(interval);
                 return;
             }
             window.scrollBy(0,50);
-
-        }, 20);
-
-    });
 }
