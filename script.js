574b771c5f17dfe41c5aed4d7c004f7343d2fa87
Shashank tyagi <shashank.tyagi1234@gmail.com>
2023-05-21 14:28:31 +0530

    Auto fill skills section bar

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Smooth scrolling code -
+
 var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
 var interval;
 for (let i =0 ; i < navMenuAnchorTags.length; i++){
@@ -11,7 +13,6 @@ for (let i =0 ; i < navMenuAnchorTags.length; i++){
 
     });
 }
-
 function scrollVertically(targetSection){
     var targetSectionCoordinates = targetSection.getBoundingClientRect();
             if(targetSectionCoordinates.top <=0){
@@ -20,3 +21,47 @@ function scrollVertically(targetSection){
             }
             window.scrollBy(0,50);
 }
+
+// Skills auto fill while scrolling animation
+
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById('skills-container');
+var animationDone = false;
+
+function initialiseBars() {
+    for (var bar of progressBars) {
+        bar.style.width = 0 + '%';
+    }
+}
+
+function fillBars() {
+
+    for (let bar of progressBars) {
+        let currentWidth = 0;
+        let interval = setInterval(function () {
+            let targetWidth = bar.getAttribute('data-bar-width');
+            if (currentWidth >= targetWidth) {
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+        }, 5);
+    }
+}
+
+function checkScroll() {
+
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if (!animationDone && coordinates.top <= window.innerHeight) {
+        animationDone = true;
+        fillBars();
+    } else if (coordinates.top > window.innerHeight) {
+        animationDone = false;
+        initialiseBars();
+    }
+}
+
+
+initialiseBars();
+window.addEventListener("scroll", checkScroll);
